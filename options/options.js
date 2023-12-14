7dc3703ac9a2ab8341d6e73cbe5a1344de1df8f2
Crowdbotics <team@crowdbotics.com>
2023-12-14 08:28:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snake-gamep-45004",
+  url: "https://snake-gamep-45004.botics.co",
+  api: "https://snake-gamep-45004.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
